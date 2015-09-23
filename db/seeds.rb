557171580b1b7e368452a534a938e3d65875a723
nathanallen <thenathanator@gmail.com>
2015-09-23 14:06:22 -0700

    added seed task

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+user = User.create(
+  email: 'test@test.com',
+  first_name: "test",
+  last_name: "subject",
+  password: "123"
+)
+
+sfpl = Library.create(
+  name: "San Francisco Public Library",
+  floor_count: 6,
+  floor_area: 100000
+)
+
+sfpl.users << user
+
+p "Finished seeding..."
+p "Created #{Library.count} libraries, #{User.count} users, #{LibraryUser.count} library patrons"
